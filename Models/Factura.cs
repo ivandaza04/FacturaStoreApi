2073b1c1f5bc0946baaf4814879bceb9c885783d
ivan <ivandaza04@gmail.com>
2022-02-24 14:51:48 -0500

    menos variables en models

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -22,27 +22,5 @@ public class Factura
     public string nit { get; set; } = null!;
 
     [BsonElement("TotalFactura")]
-    public decimal totalFactura { get; set; }
-
-    [BsonElement("SubTotal")]
-    public decimal subTotal { get; set; }
-
-    [BsonElement("Iva")]
-    public decimal iva { get; set; }
-
-    [BsonElement("Retencion")]
-    public decimal retencion { get; set; }
-
-    [BsonElement("FechaCreacion")]
-    public string fechaCreacion { get; set; } = null!;
-
-    [BsonElement("Estado")]
-    public string estado { get; set; } = null!;
-
-    [BsonElement("Pagada")]
-    public Boolean pagada { get; set; }
-
-    [BsonElement("FechaPago")]
-    public string fechaPago { get; set; } = null!;
-
+    public string totalFactura { get; set; } = null!;
 }
