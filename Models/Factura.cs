ba0772a15d6c8bad9aebb730d66b1df342aa0557
ivan <ivandaza04@gmail.com>
2022-02-26 11:54:49 -0500

    con id tipo object

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -5,8 +5,9 @@ namespace FacturaStoreApi.Models;
 
 public class Factura
 {
-    [BsonElement("Id")]
-    public string Id { get; set; } = null!;
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
     [BsonElement("CodigoFactura")]
     public string codigoFactura { get; set; } = null!;
