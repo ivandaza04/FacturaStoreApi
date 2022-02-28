6a1d91f01bf0c1fcbcd2a6172e15297ae98b58f4
ivan <ivandaza04@gmail.com>
2022-02-28 11:18:02 -0500

    nuevos datos

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -22,8 +22,23 @@ public class Factura
     public string nit { get; set; } = null!;
 
     [BsonElement("TotalFactura")]
-    public string totalFactura { get; set; } = null!;
+    public decimal  totalFactura { get; set; }
+
+    [BsonElement("SubTotal")]
+    public decimal subTotal { get; set; }
+
+    [BsonElement("Iva")]
+    public decimal iva { get; set; }
+
+    [BsonElement("Retencion")]
+    public decimal retencion { get; set; }
+
+    [BsonElement(" FechaCreacion")]
+    public DateTime fechaCreacion { get; set; } = DateTime.Now;
 
     [BsonElement("Estado")]
     public string estado { get; set; } = null!;
+
+    [BsonElement(" Pagada")]
+    public Boolean pagada { get; set; }
 }
